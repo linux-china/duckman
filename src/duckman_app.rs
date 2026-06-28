8520404f3219bb389f816383c01fe5c52c383761
linux_china <libing.chen@gmail.com>
2026-06-28 08:50:34 +0800

    release: update to version 0.1.6

--- src/duckman_app.rs
+++ src/duckman_app.rs
@@ -1,6 +1,6 @@
 use clap::{Arg, ArgAction, Command};
 
-pub const VERSION: &str = "0.1.7";
+pub const VERSION: &str = "0.1.6";
 
 pub fn build_duckman_app() -> Command {
     let run_command = Command::new("run")
